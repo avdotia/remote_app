cdaf42e1bac8b18adcb57d7f1531ab40231b72b6
Laura <lauraperezfelipe@gmail.com>
2013-10-06 19:40:22 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RemeroApp::Application.config.secret_token = 'cec3eafdb3c862b28cca2c148f9cc0dec32a26493dff0e1d2c3bf2ed38b440a5717bc63695073650f2222da09f34177c8ec10ea34ce72c776dbfedd712e7f3f4'
